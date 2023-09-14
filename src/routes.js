8d52117ab73c473207b1c55bd21e07687506752b
luanavfg <luanavefago1@gmail.com>
2023-09-14 15:21:41 -0300

    feat: add routes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
-import { Database } from "./database";
+import { Database } from "./database.js";
 import { randomUUID } from "node:crypto";
+import { buildRoutePath } from "./utils/build-route-path.js";
 
 const database = new Database();
 
 export const routes = [
   {
     method: "POST",
-    path: "/tasks",
+    path: buildRoutePath("/tasks"),
     handler: (req, res) => {
       const { title, description } = req.body;
       const task = {
@@ -23,7 +24,7 @@ export const routes = [
   },
   {
     method: "GET",
-    path: "/tasks",
+    path: buildRoutePath("/tasks"),
     handler: (req, res) => {
       const { search } = req.query;
       const tasks = database.select(
@@ -31,6 +32,41 @@ export const routes = [
         search ? { title: search, description: search } : null
       );
       return res.end(JSON.stringify(tasks));
+    },
+  },
+  {
+    method: "PUT",
+    path: buildRoutePath("/tasks/:id"),
+    handler: (req, res) => {
+      const { id } = req.params;
+      const { title, description } = req.body;
+      const updated_at = new Date();
+      if (title) {
+        database.update("tasks", id, { title, updated_at });
+        return res.writeHead(204).end();
+      }
+      if (description) {
+        database.update("tasks", id, { description, updated_at });
+        return res.writeHead(204).end();
+      }
+    },
+  },
+  {
+    method: "DELETE",
+    path: buildRoutePath("/tasks/:id"),
+    handler: (req, res) => {
+      const { id } = req.params;
+      database.delete("tasks", id);
+      return res.writeHead(204).end();
+    },
+  },
+  {
+    method: "PATCH",
+    path: buildRoutePath("/tasks/:id/complete"),
+    handler: (req, res) => {
+      const { id } = req.params;
+      database.updateStatus("tasks", id);
+      return res.writeHead(204).end();
     },
   },
 ];
