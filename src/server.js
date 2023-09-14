0da496698bf46ddcc6fa2df4255e2dcd6f808bb4
luanavfg <luanavefago1@gmail.com>
2023-09-14 15:21:51 -0300

    adjust: adjust server

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import { extractQueryParams } from "./utils/extract-query-params.js";
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
-  // await json(req, res);
+  await json(req, res);
 
   const route = routes.find((route) => {
     return route.method === method && route.path.test(url);
@@ -15,7 +15,8 @@ const server = http.createServer(async (req, res) => {
   if (route) {
     const routeParams = req.url.match(route.path);
     const { query, ...params } = routeParams.groups;
-    req.params = paramsreq.query = query ? extractQueryParams(query) : {};
+    req.params = params;
+    req.query = query ? extractQueryParams(query) : {};
 
     return route.handler(req, res);
   }
